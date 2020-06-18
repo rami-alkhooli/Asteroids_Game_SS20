371f40fd4fd8b7ca80a09b1860aa1acdf91dc3e5
rami-alkhooli <rami.al.kholi@gmail.com>
2020-06-18 20:04:51 +0200

    Dokumentation von AccelerationStates

--- AsteroidsGame/AccStateReturning.java
+++ AsteroidsGame/AccStateReturning.java
@@ -2,6 +2,13 @@ import processing.core.*;
 
 public class AccStateReturning implements AccelerationState
 {
+  /**
+  * This function increases the speed of the space ship while accelerating backwards
+  * 
+  * @param speed holds the value of the space ship
+  * @param size holds the size of the space ship and indicates the mass of the it so that the acceleration is relative to the mass
+  * @returns the new speed after increasing it
+  **/
   public float accelerate(float speed, float size) {
      
     float ACCELERATING_STEP = (float) (size/300);
@@ -13,6 +20,16 @@ public class AccStateReturning implements AccelerationState
   
   }
   
+  /**
+  * This function is overwritten and does nothing because when accelerating backwards a thrust won't be generated
+  *
+  * @param theApp is a pointer on the processing object 
+  * @param x holds the position of the midpoint of the space ship on the x axis
+  * @param y holds the position of the midpoint of the space ship on the y axis
+  * @param up holds the length of the space ship from the midpoint to the top
+  * @param down holds the length of the space ship from the midpoint to the bottom
+  * @param angle holds the current angle of the space ship
+  **/
   public void generateThrust(PApplet app, float x, float y, float up, float down, float angle) {
     
   }
--- AsteroidsGame/AccStateStoppedF.java
+++ AsteroidsGame/AccStateStoppedF.java
@@ -2,6 +2,13 @@ import processing.core.*;
 
 public class AccStateStoppedF implements AccelerationState
 {
+  /**
+  * This function decreases the speed of the space ship while decelerating forwards
+  * 
+  * @param speed holds the value of the space ship
+  * @param size holds the size of the space ship and indicates the mass of the it so that the deceleration is relative to the mass
+  * @returns the new speed after decreasing it
+  **/
   public float accelerate(float speed, float size) {
     
     float ACCELERATING_STEP = (float) (size/300);
@@ -14,6 +21,16 @@ public class AccStateStoppedF implements AccelerationState
   
   }
   
+  /**
+  * This function is overwritten and does nothing because when the space ship stops accelerating a thrust won't be generated
+  *
+  * @param theApp is a pointer on the processing object 
+  * @param x holds the position of the midpoint of the space ship on the x axis
+  * @param y holds the position of the midpoint of the space ship on the y axis
+  * @param up holds the length of the space ship from the midpoint to the top
+  * @param down holds the length of the space ship from the midpoint to the bottom
+  * @param angle holds the current angle of the space ship
+  **/
   public void generateThrust(PApplet app, float x, float y, float up, float down, float angle) {
     
   }
